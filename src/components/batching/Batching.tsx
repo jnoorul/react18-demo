97217fb6f804751da6bf2b47f18841e1cf59802f
Noorul Ameen <noorul@nooruls-air.lan>
2023-08-19 17:10:35 +0800

    initial version of batching

--- src/components/batching/Batching.tsx
+++ src/components/batching/Batching.tsx
@@ -1,3 +1,36 @@
+import { useState } from 'react';
+import { CardList } from './CardList';
+
 export function Batching() {
-  return <div>Batching</div>;
+  const [pageNumber, setPageNumber] = useState(1);
+  const [numberOfRows, setNumberOfRows] = useState(5);
+  const loadMore = () => {
+    setTimeout(() => {
+      setPageNumber(pageNumber + 1);
+      setNumberOfRows(pageNumber * 5);
+    }, 10);
+  };
+
+  return (
+    <div className="m-6">
+      <ExpensiveComponent />
+      <button
+        className="border border-red-800 p-2 rounded-md m-4"
+        onClick={() => loadMore()}
+      >
+        Load more...
+      </button>
+      <h3>{`Current Page: ${pageNumber}`}</h3>
+      <h3>{`Number of Rows: ${numberOfRows}`}</h3>
+      {[...Array(pageNumber).keys()].map(() => (
+        <CardList />
+      ))}
+    </div>
+  );
+}
+
+function ExpensiveComponent() {
+  console.log('Rendering...');
+  for (let i = 0; i < 1000000000; i++) {}
+  return null;
 }
