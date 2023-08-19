a13e3231d06954480929a2a404dbea438547db7a
NOORULAMEEN JAMAL MOHAMED <noorul.it@gmail.com>
2023-08-19 19:56:41 +0800

    refactor concurrent

--- src/components/concurrent/Concurrent.tsx
+++ src/components/concurrent/Concurrent.tsx
@@ -2,18 +2,20 @@ import { ChangeEvent, useState, useTransition } from 'react';
 import { CardList } from '../batching/CardList';
 
 export function Concurrent() {
-  const [, startTransition] = useTransition();
+  const [isPending, startTransition] = useTransition();
   const [searchText, setSearchText] = useState('');
   const [noOfCards, setNoOfCards] = useState(10);
 
   function onSearch(event: ChangeEvent<HTMLInputElement>) {
     const newSearchText = event.currentTarget.value;
+    const noOfResults = 20 - newSearchText.length;
     setSearchText(newSearchText);
-    startTransition(() => setNoOfCards(10 - newSearchText.length));
+    startTransition(() => setNoOfCards(noOfResults));
+    // setNoOfCards(noOfResults);
   }
 
   return (
-    <div className="m-8 border-2">
+    <div className="m-8">
       <input
         type="text"
         id="search"
@@ -22,6 +24,7 @@ export function Concurrent() {
         value={searchText}
         onChange={onSearch}
       />
+      {isPending && <>Loading...</>}
       <CardList noOfCards={noOfCards} />
     </div>
   );
