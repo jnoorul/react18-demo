019c0464e1041df04b3d4b29b5c6bd8a00b901e5
NOORULAMEEN JAMAL MOHAMED <noorul.it@gmail.com>
2023-08-20 21:27:41 +0800

    use menu bar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../header/Header';
 import { NavBar } from '../navbar/NavBar';
+import MenuBar from '../navbar/MenuBar';
 
 export function Layout() {
   return (
     <div className="h-full">
       <Header />
+      <MenuBar />
       <div className="flex flex-row h-full">
         <NavBar />
         <Outlet />
